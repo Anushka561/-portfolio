f5d2ef2c3cc52b5d91959a7d2f978b16090a1ff8
anushka <anushka3935@gmail.com>
2023-02-03 03:56:36 +0530

    commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ import "./header.css";
 
 const Header = () => {
   return (
-    <header id="header>
+    <header id="header">
       <div className="container header__container">
 
         <h5>Hello i'm </h5>
